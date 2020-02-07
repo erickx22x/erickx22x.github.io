8856f7076ce1cdeb2f5a5d9838a44ad69b30e21a
Érick Almeida <55899922+erickx22x@users.noreply.github.com>
2020-02-07 17:34:13 -0300

    Update arquivo.js

--- JS31012020/arquivo.js
+++ JS31012020/arquivo.js
@@ -1,3 +1,32 @@
-function disparar_alerta()
-{alert("Olá Mundo, meu primeiro JS");
+function alerta()
+{
+  alert("Teste de alerta");
+};
+
+function confirma()
+{
+  confirm("confirmado");
+};
+
+function prompt1()
+{
+  invert = prompt("Digite uma palavra:");
+
+  (invert == reverterStr(invert) ? alert("é um palíndromo") : alert("não é um palíndromo"));
+};
+
+function reverterStr(str)
+{
+    return str.split("").reverse().join("");
+};
+
+function fatorial(num)
+{
+  return num <= 0 ? 1 : num*(fatorial(num-1));
+};
+
+function fato_prom()
+{
+  num = parseInt(prompt("Digite um número inteiro:"));
+  alert(fatorial(num));
 };

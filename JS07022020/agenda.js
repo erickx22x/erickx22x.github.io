8c19041fd15ac33b006ff5d07fd3233e387ffd3c
Érick Almeida <55899922+erickx22x@users.noreply.github.com>
2020-02-07 21:56:06 -0300

    Update agenda.js

--- JS07022020/agenda.js
+++ JS07022020/agenda.js
@@ -2,7 +2,17 @@ class Agenda{
 	constructor (){
 		this.contato = [];
 	} 	
+	criar(){
+	      var nome = prompt("Informe o Nome!");
+	      var sobrenome = prompt("Informe o Sobrenome!");
+	      var nascimento = prompt("Informe o Nascimento!");
+	      var tipo = prompt("Informe o Tipo do telefone!");
+	      var ddd = prompt("Informe o DDD!");
+	      var num = prompt("Informe o Numero!");
 
+	      this.adicionarContato(nome, sobrenome, nascimento, tipo, ddd, num);
+	}
+	
 	adicionarContato(nome,sobrenome,idade,tipo,ddd,num){
 		this.contato.push(new Contato(new Pessoa(nome,sobrenome,idade)),new Numero(tipo,ddd,num));
 	}

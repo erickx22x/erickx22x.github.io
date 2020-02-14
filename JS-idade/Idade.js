248cbdbd06e1304f564caa7c84c8c44aa47b7786
Érick Almeida <55899922+erickx22x@users.noreply.github.com>
2020-02-14 17:03:52 -0300

    Update Idade.js

--- JS-idade/Idade.js
+++ JS-idade/Idade.js
@@ -6,5 +6,5 @@ function Calcular(){
 function calcularIdade(birthday) {
     var dob_timestamp = new Date(birthday).getTime();
     var age = ((new Date()).getTime() - dob_timestamp) / (31556926000);
-    document.getElementById("resultado").innerHTML = (age.toFixed(1)) + " Anos";
+    document.getElementById("result").innerHTML = (age.toFixed(1)) + " Anos";
 }

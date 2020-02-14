bb78fb21d440bd21fb6addaa74f24b9d226634e8
Érick Almeida <55899922+erickx22x@users.noreply.github.com>
2020-02-14 18:05:38 -0300

    Update Idade.js

--- JS-idade/Idade.js
+++ JS-idade/Idade.js
@@ -1,10 +1,11 @@
-function Calcular(){
-    var i = window.prompt("Digite o ano de nascimento");
-    calcularIdade(i);
-}
+function Idade()
+{
+  d = prompt("Informe o dia do seu nascimento");
+  m = prompt("Informe o mes do seu nascimento");
+  y = prompt("Informe o ano do seu nascimento");
 
-function calcularIdade(birthday) {
-    var dob_timestamp = new Date(birthday).getTime();
-    var age = ((new Date()).getTime() - dob_timestamp) / (31556926000);
-    document.getElementById("result").innerHTML = (age.toFixed(1)) + " Anos";
+  nasc = new Date(y, m, d);
+  idade = Math.floor((Date.now() - nasc)/31557600000);
+
+  alert("Sua idade é " + idade);
 }
